88a53195b6e3238d9538ba4fdcd60e7dd2c24ca0
jared strate <strate.jared@gmail.com>
2009-11-28 23:11:48 -0800

    project file for lein

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject crane "1.0-SNAPSHOT"
+  :description "heavy lifting for ec2"
+  :dependencies [[org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [net.java.dev.jets3t/jets3t "0.7.1"]
+                 [com.google.code.typica/typica "1.5.2a"]
+                 [com.jcraft/jsch "0.1.42"]
+                 [jline "0.9.94"]]
+  :dev-dependencies [[org.clojure/swank-clojure "1.0"]])
