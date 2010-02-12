e6430896a916e69550c740dd6567bff2401f4049
Mark McGranaghan <mmcgrana@gmail.com>
2010-02-12 12:05:18 -0500

    Update project.clj for current Clojure versions and recent jclouds merge.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject crane "1.0-SNAPSHOT"
   :description "heavy lifting for ec2"
-  :dependencies [[org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
                  [net.java.dev.jets3t/jets3t "0.7.1"]
                  [com.google.code.typica/typica "1.5.2a"]
                  [org.jclouds/jclouds-blobstore "1.0-SNAPSHOT"]
@@ -14,4 +15,6 @@
                  [org.jclouds/jclouds-rimuhosting "1.0-SNAPSHOT"]
                  [com.jcraft/jsch "0.1.42"]
                  [jline "0.9.94"]]
-  :dev-dependencies [[org.clojure/swank-clojure "1.0"]])
+  :dev-dependencies [[org.clojure/swank-clojure "1.0"]]
+  :repositories [["jclouds" "http://jclouds.googlecode.com/svn/repo"]
+                 ["jclouds-snapshot" "http://jclouds.rimuhosting.com/maven2/snapshots"]])
