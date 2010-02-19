f30be97569b63154c89a6e67ed831a3d207208d9
Hugo Duncan <hugo_duncan@yahoo.com>
2010-02-19 10:38:43 -0500

    clean up of type hints. made run-nodes and metadata functions return sequence of domain objects, rather than immutable set entries

--- src/crane/compute.clj
+++ src/crane/compute.clj
@@ -1,6 +1,5 @@
 (ns
-#^{:doc
-"
+    #^{:doc "
 a lib for interacting with jclouds ComputeService.
 
 Current supported services are:
@@ -25,19 +24,15 @@ Here's an example of getting some compute configuration from rackspace:
 
 "}
   crane.compute
-
   (:use clojure.contrib.duck-streams)
   (:import java.io.File
            org.jclouds.domain.Location
            (org.jclouds.compute ComputeService
                                 ComputeServiceContext
                                 ComputeServiceContextFactory)
-           (org.jclouds.compute.domain Template
-                                       TemplateBuilder
-                                       ComputeMetadata
-                                       Size
-                                       OsFamily
-                                       Image)))
+           (org.jclouds.compute.domain Template TemplateBuilder ComputeMetadata
+                                       NodeMetadata Size OsFamily Image)
+           (com.google.common.collect ImmutableSet)))
 
 (def module-lookup
      {:log4j org.jclouds.logging.log4j.config.Log4JLoggingModule
@@ -50,44 +45,42 @@ Here's an example of getting some compute configuration from rackspace:
   (.build #^com.google.common.collect.ImmutableSet$Builder
           (reduce #(.add #^com.google.common.collect.ImmutableSet$Builder %1
                          (.newInstance #^Class (%2 module-lookup)))
-                  (#^com.google.common.collect.ImmutableSet$Builder
-                   com.google.common.collect.ImmutableSet/builder)
+                  (com.google.common.collect.ImmutableSet/builder)
                   modules)))
-
 
 (defn compute-context
   "Create a logged in context."
   ([s a k]
      (compute-context s a k (modules :log4j :ssh :enterprise)))
-  ([#^String s #^String a #^String k #^com.google.common.collect.ImmutableSet m]
+  ([#^String s #^String a #^String k #^ImmutableSet m]
      (.createContext (new ComputeServiceContextFactory) s a k m)))
+
+(defn- seq-from-immutable-set [#^ImmutableSet set]
+  (map #(.getValue %) set))
 
 (defn locations
   "Retrieve the available compute locations for the compute context."
-  [#^org.jclouds.compute.ComputeServiceContext compute]
-  (.getLocations (.getComputeService compute)))
-
-(defn node-list [node-set]
-  (map #(.getValue %) node-set))
+  [#^ComputeServiceContext compute]
+  (seq-from-immutable-set (.getLocations (.getComputeService compute))))
 
 (defn nodes
   "Retrieve the existing nodes for the compute context."
-  ([#^org.jclouds.compute.ComputeServiceContext compute]
-     (node-list (.getNodes (.getComputeService compute))))
-  ([#^org.jclouds.compute.ComputeServiceContext compute #^String tag]
-     (node-list (.getNodesWithTag (.getComputeService compute) tag))))
+  ([#^ComputeServiceContext compute]
+     (seq-from-immutable-set (.getNodes (.getComputeService compute))))
+  ([#^ComputeServiceContext compute #^String tag]
+     (seq-from-immutable-set (.getNodesWithTag (.getComputeService compute) tag))))
 
 (defn images
   "Retrieve the available images for the compute context."
-  [#^org.jclouds.compute.ComputeServiceContext compute]
-  (.getImages (.getComputeService compute)))
+  [#^ComputeServiceContext compute]
+  (seq-from-immutable-set (.getImages (.getComputeService compute))))
 
 (defn sizes
   "Retrieve the available node sizes for the compute context."
-  [#^org.jclouds.compute.ComputeServiceContext compute]
-  (.getSizes (.getComputeService compute)))
+  [#^ComputeServiceContext compute]
+  (seq-from-immutable-set (.getSizes (.getComputeService compute))))
 
-(defn default-template [#^org.jclouds.compute.ComputeServiceContext compute]
+(defn default-template [#^ComputeServiceContext compute]
   (.. compute (getComputeService) (templateBuilder)
     (osFamily OsFamily/UBUNTU)
     smallest
@@ -99,77 +92,78 @@ Here's an example of getting some compute configuration from rackspace:
 (defn run-nodes
   "Create the specified number of nodes using the default or specified
    template."
-  ([#^org.jclouds.compute.ComputeServiceContext compute tag count]
-    (.runNodesWithTag
-     (.getComputeService compute) tag count (default-template compute)))
-  ([#^org.jclouds.compute.ComputeServiceContext compute tag count template]
-    (.runNodesWithTag
-     (.getComputeService compute) tag count template)))
+  ([compute tag count]
+     (run-nodes compute tag count (default-template compute)))
+  ([#^ComputeServiceContext compute tag count template]
+     (seq-from-immutable-set
+      (.runNodesWithTag
+       (.getComputeService compute) tag count template))))
 
 (defn run-node
-  "Create the specified number of nodes using the default context."
-  ([#^org.jclouds.compute.ComputeServiceContext compute tag]
-    (run-nodes compute tag 1))
-  ([#^org.jclouds.compute.ComputeServiceContext compute tag template]
-    (run-nodes compute tag 1 template)))
+  "Create a node using the default or specified template."
+  ([compute tag]
+    (run-nodes compute tag 1 (default-template compute)))
+  ([compute tag template]
+     (run-nodes compute tag 1 template)))
 
-(defn node-details
-  "Retrieve the node metadata"
-  [#^org.jclouds.compute.ComputeServiceContext compute node]
+(defn #^NodeMetadata node-details
+  "Retrieve the node metadata."
+  [#^ComputeServiceContext compute node]
   (.getNodeMetadata (.getComputeService compute) node ))
 
 (defn reboot-nodes
   "Reboot all the nodes with the given tag."
-  ([#^org.jclouds.compute.ComputeServiceContext compute #^String tag]
+  ([#^ComputeServiceContext compute #^String tag]
     (.rebootNodesWithTag (.getComputeService compute) tag )))
 
 (defn reboot-node
   "Reboot a given node."
-  ([#^org.jclouds.compute.ComputeServiceContext compute
-    #^org.jclouds.compute.domain.ComputeMetadata node]
+  ([#^ComputeServiceContext compute
+    #^ComputeMetadata node]
     (.rebootNode (.getComputeService compute) node )))
 
 (defn destroy-nodes
   "Destroy all the nodes with the given tag."
-  ([#^org.jclouds.compute.ComputeServiceContext compute #^String tag]
+  ([#^ComputeServiceContext compute #^String tag]
     (.destroyNodesWithTag (.getComputeService compute) tag )))
 
 (defn destroy-node
   "Destroy a given node."
-  ([#^org.jclouds.compute.ComputeServiceContext compute
-    #^org.jclouds.compute.domain.ComputeMetadata node]
+  ([#^ComputeServiceContext compute
+    #^ComputeMetadata node]
     (.destroyNode (.getComputeService compute) node )))
 
 (defmacro state-predicate [node state]
-  `(= (.getState ~node) (. org.jclouds.compute.domain.NodeState ~state)))
+  `(= (.getState ~node)
+      (. org.jclouds.compute.domain.NodeState ~state)))
 
 (defn pending?
   "Predicate for the node being in transition"
-  [node]
+  [#^NodeMetadata node]
   (state-predicate node PENDING))
 
 (defn running?
   "Predicate for the node being available for requests."
-  [node]
+  [#^NodeMetadata node]
   (state-predicate node RUNNING))
 
 (defn terminated?
   "Predicate for the node being halted."
-  [node]
+  [#^NodeMetadata node]
   (state-predicate node TERMINATED))
 
 (defn suspended?
   "Predicate for the node being suspended."
-  [node]
+  [#^NodeMetadata node]
   (state-predicate node SUSPENDED))
 
 (defn error-state?
   "Predicate for the node being in an error state."
-  [node]
+  [#^NodeMetadata node]
   (state-predicate node ERROR))
 
 (defn unknown-state?
   "Predicate for the node being in an unknown state."
-  [node]
+  [#^NodeMetadata node]
   (state-predicate node UNKNOWN))
 
